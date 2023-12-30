752e8ada0e359b0f96c7e998791a5299fd09d585
Unxpected155 <gabripb2004@gmail.com>
2023-12-30 13:37:11 -0600

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,6 +4,8 @@
  * @returns the url string cropped
  */
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return "";
+
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
